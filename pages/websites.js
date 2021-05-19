0db5e656ddfda107aa548c934ecef981b28a117d
pqoweo10 <evanengler6@gmail.com>
2021-05-19 17:13:50 -0400

    Update

--- pages/websites.js
+++ pages/websites.js
@@ -8,6 +8,58 @@ export default function Websites() {
             <title>Websites | Customer Portal | GearedUp</title>
             <link rel="icon" href="https://static.wixstatic.com/media/6a5579_af1b4ee821284e218d2e24416ca2b250~mv2.jpg/v1/crop/x_0,y_0,w_115,h_128/fill/w_48,h_53,al_c,q_80,usm_0.66_1.00_0.01/GearedUp_Logo_Final.webp" /> 
             </Head>
+
+        <main className={styles.main}>
+            <h1 className={styles.title}>
+            Welcome to the <a href="https://gearedup.zone">GearedUp</a> customer portal!
+            </h1>
+
+            <p className={styles.description}>
+            What can we do for you today?
+            </p>
+
+            <div className={styles.grid}>
+            <a href="../pages/websites.js" className={styles.card}>
+                <h3>Websites &rarr;</h3>
+                <p>View all your previous and current websites.</p>
+            </a>
+
+            <a href="https://nextjs.org/learn" className={styles.card}>
+                <h3>Payments &rarr;</h3>
+                <p>View your payment history, or make a payment.</p>
+            </a>
+
+            <a
+                href="https://github.com/vercel/next.js/tree/master/examples"
+                className={styles.card}
+            >
+                <h3>Data &rarr;</h3>
+                <p>View/Edit all of your data we have.</p>
+            </a>
+
+            <a
+                href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                className={styles.card}
+            >
+                <h3>Create a Ticket &rarr;</h3>
+                <p>
+                Found a problem with the portal? Let us know.
+                </p>
+            </a>
+            </div>
+        </main>
+
+            
+            <footer className={styles.footer}>
+        <a
+          href="https://www.gearedup.zone/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="https://static.wixstatic.com/media/6a5579_af1b4ee821284e218d2e24416ca2b250~mv2.jpg/v1/crop/x_0,y_0,w_115,h_128/fill/w_48,h_53,al_c,q_80,usm_0.66_1.00_0.01/GearedUp_Logo_Final.webp" alt="GearedUp Logo" className={styles.logo} />
+        </a>
+            </footer>
         </div>
     )
 }
