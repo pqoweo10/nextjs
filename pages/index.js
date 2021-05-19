e5db601572098ad497c07d47fbaef91ca8c787eb
pqoweo10 <evanengler6@gmail.com>
2021-05-19 14:25:27 -0400

    GearedUp Logo transfer

--- pages/index.js
+++ pages/index.js
@@ -52,12 +52,12 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.gearedup.zone/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/vercel.svg" alt="GearedUp Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
